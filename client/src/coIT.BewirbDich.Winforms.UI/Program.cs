9d77ceba08f03b81c4028d7b279bf30206456230
Nicolai Pischel <nicolai.pischel@live.de>
2023-02-04 16:37:55 +0100

    Introducing DI to be able to proper inject the api client into the winforms client

--- client/src/coIT.BewirbDich.Winforms.UI/Program.cs
+++ client/src/coIT.BewirbDich.Winforms.UI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Hosting;
+
 namespace coIT.BewirbDich.Winforms.UI;
 
 static class Program
@@ -11,6 +13,13 @@ static class Program
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
+
+        var builder = Host.CreateDefaultBuilder();
+        var app = builder.Build();
+        
+        ServiceProvider = app.Services;
+        
         Application.Run(new Form1());
     }
+    internal static IServiceProvider ServiceProvider { get; private set; }
 }
