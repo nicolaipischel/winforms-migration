bd70e8679df284af9f79fd91d545ec8133af8d69
Nicolai Pischel <nicolai.pischel@live.de>
2023-02-04 16:49:24 +0100

    Creating validator for the options
    So no wrong values in the appsettings.json will mess up the the system

---
+++ client/src/coIT.BewirbDich.Winforms.UI/Configuration/OptionsValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Options;
+
+namespace coIT.BewirbDich.Winforms.UI.Configuration;
+
+internal class OptionsValidator<TOptions> : AbstractValidator<TOptions>, IValidateOptions<TOptions> where TOptions : class
+{
+    public ValidateOptionsResult Validate(string? name, TOptions options)
+    {
+        var result = Validate(options);
+        if (result.IsValid) return ValidateOptionsResult.Success;
+
+        var errors = result.Errors.Select(error => error.ErrorMessage);
+        return ValidateOptionsResult.Fail(errors);
+    }
+}
